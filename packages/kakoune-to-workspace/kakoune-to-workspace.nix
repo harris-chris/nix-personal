aed654eb050cc95f7a1fd0574236691f958f60b9
harris-chris <chrisharriscjh@gmail.com>
2021-12-26 16:42:18 +0900

    Working OK

--- packages/kakoune-to-workspace/kakoune-to-workspace.nix
+++ packages/kakoune-to-workspace/kakoune-to-workspace.nix
@@ -6,18 +6,12 @@ let
 
     desktop="$(${getworkspacename}/bin/getworkspacename)"
 
-    echo "RAN COMMAND"
-    echo ${getworkspacename}/bin/getworkspacename
-    echo $desktop
-
     # bspc result was empty, so most likely not using bspwm
     [ -z "$desktop" ] && exec ${kakoune}/bin/kak "$@"
 
     ${kakoune}/bin/kak -clear
 
     # if session with desktop id is found, connect to it. otherwise create it
-    echo $(${kakoune}/bin/kak -l | grep -q "^''${desktop}$")
-    echo ${kakoune}/bin/kak -s $desktop $@
     if ${kakoune}/bin/kak -l | grep -q "^''${desktop}$"; then
         exec ${kakoune}/bin/kak -c $desktop $@
     else
